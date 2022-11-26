9e5b56f7fcc1a55884211f6f7619b6c559a0ab83
Sai Prakash <63905195+SaiSpr@users.noreply.github.com>
2022-11-26 21:41:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ st.sidebar.subheader('Heat map parameter')
 time_hist_color = st.sidebar.selectbox('Color by', ('temp_min', 'temp_max')) 
 
 st.sidebar.subheader('Donut chart parameter')
-donut_theta = st.sidebar.selectbox('Select data', ('q2', 'q3'))
+donut_theta = st.sidebar.selectbox('Select Data', ('q2', 'q3'))
 
 st.sidebar.subheader('Line chart parameters')
 plot_data = st.sidebar.multiselect('Select data', ['temp_min', 'temp_max'], ['temp_min', 'temp_max'])
@@ -30,31 +30,31 @@ col2.metric("Wind", "11 kmph", "-8%")
 col3.metric("Humidity", "86%", "4%")
 
 # Row B
-seattle_weather = pd.read_csv('https://github.com/SaiSpr/Streamlit-Dashboard/blob/main/weather_data.csv', parse_dates=['date'])
-stocks = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/stocks_toy.csv')
+weather = pd.read_csv('https://raw.githubusercontent.com/tvst/plost/master/data/seattle-weather.csv', parse_dates=['date'])
+stocks = pd.read_csv('https://github.com/SaiSpr/Streamlit-Dashboard/blob/main/demo_stocks.csv')
 
 c1, c2 = st.columns((7,3))
 with c1:
     st.markdown('### Heatmap')
     plost.time_hist(
-    data=seattle_weather,
-    date='date',
-    x_unit='week',
-    y_unit='day',
-    color=time_hist_color,
-    aggregate='median',
-    legend=None,
-    height=345,
-    use_container_width=True)
+    data = weather,
+    date = 'date',
+    x_unit = 'week',
+    y_unit = 'day',
+    color = time_hist_color,
+    aggregate = 'median',
+    legend = None,
+    height = 345,
+    use_container_width = True)
 with c2:
     st.markdown('### Donut chart')
     plost.donut_chart(
-        data=stocks,
-        theta=donut_theta,
-        color='company',
-        legend='bottom', 
-        use_container_width=True)
+        data = stocks,
+        theta = donut_theta,
+        color = 'company',
+        legend = 'bottom', 
+        use_container_width = True)
 
 # Row C
 st.markdown('### Line chart')
-st.line_chart(seattle_weather, x = 'date', y = plot_data, height = plot_height)
+st.line_chart(weather, x = 'date', y = plot_data, height = plot_height)
