cb8b9b6eaa70709f9be9fa774d3512e7e08ac3b6
Sai Prakash <63905195+SaiSpr@users.noreply.github.com>
2022-11-26 22:04:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -85,18 +85,16 @@ st.sidebar.subheader('Line chart parameters')
 plot_data = st.sidebar.multiselect('Select data', ['temp_min', 'temp_max'], ['temp_min', 'temp_max'])
 plot_height = st.sidebar.slider('Specify plot height', 200, 500, 250)
 
-st.sidebar.markdown('''
----
-Created with ❤️ by [Data Professor](https://youtube.com/dataprofessor/).
-''')
+
 
 
 # Row A
 st.markdown('### Metrics')
 col1, col2, col3 = st.columns(3)
-col1.metric("Temperature", "70 °F", "1.2 °F")
-col2.metric("Wind", "9 mph", "-8%")
+col1.metric("Temperature", "20 °C", "2 °C")
+col2.metric("Wind", "11 kmph", "-8%")
 col3.metric("Humidity", "86%", "4%")
+
 
 # Row B
 seattle_weather = pd.read_csv('https://raw.githubusercontent.com/tvst/plost/master/data/seattle-weather.csv', parse_dates=['date'])
